32278750e46d8e9d6d87dfc63ae9300002ad8dc9
Josué Junio <josue.fa2010@gmail.com>
2025-04-19 12:25:45 -0300

    Feat: implements database connection

--- config/db.go
+++ config/db.go
@@ -16,47 +16,49 @@ func InitializeDb() (*gorm.DB, error) {
 	var err error
 	logger = GetLogger("db")
 
-	db, err = openDb()
+	dsn := defineDSN()
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		logger.ErrorF("Error opening database: %v", err)
 		return nil, err
 	}
+
+	migrateURL := buildMigrateURL()
 	m, err := migrate.New(
 		"file://infra/database/migrations",
-		buildMigrateURL(),
+		migrateURL,
 	)
 	if err != nil {
-		logger.ErrorF("Error creating migrations: %v", err)
+		logger.ErrorF("Error creating migration instance: %v", err)
+		return db, err
 	}
 
 	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		logger.ErrorF("Cannot run migrations: %v", err)
+		logger.ErrorF("Error applying migrations: %v", err)
+		return db, err
 	}
 
 	return db, nil
 }
 
-func openDb() (*gorm.DB, error) {
-	var err error
-
-	db, err = gorm.Open(postgres.New(postgres.Config{
-		DSN: defineDNS(),
-	}), &gorm.Config{})
-
-	return db, err
-}
-
-func defineDNS() string {
-	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%v TimeZone=America/Sao_Paulo",
-		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), "1234",
-		os.Getenv("POSTGRES_DB"), "5432")
+func defineDSN() string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s TimeZone=America/Sao_Paulo",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		"5432",
+	)
 }
 
 func buildMigrateURL() string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DB"))
+		"5432",
+		os.Getenv("POSTGRES_DB"),
+	)
 }
