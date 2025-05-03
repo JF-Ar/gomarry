15ab3d41cb3804adcf32b5a9c77c96f6d2b7e4c2
JFAR Solutions <josue.fa2010@gmail.com>
2025-05-03 14:11:49 -0300

    feat: implements handle errors

---
+++ helper/constants/errorsConstants.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	PasswordTooShortErrorMessage = "Password is too short"
+	InvalidFormatEmail           = "Invalid email. Please use a valid email format."
+	InvalidRole                  = "Invalid role. Please use a valid role"
+)
